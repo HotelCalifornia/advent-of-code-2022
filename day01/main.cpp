ad15974802aedbf6a05ea48092e3ca2023fd2e5a
Hannah Brooke <hannah@mail.yttrium.io>
2022-12-01 16:00:11 -0600

    initial commit; day 01

---
+++ day01/main.cpp
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <vector>
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " input_file" << std::endl;
+        return -1;
+    }
+
+	std::ifstream input(argv[1]);
+    if (not input) {
+        std::cerr << "Error: could not open file " << argv[1] << std::endl;
+    }
+
+    std::vector<int> rations;
+    int ration_accumulator = 0;
+    
+    // each elf's ration list is separated from the others by an extra blank
+    // line, so accumulate values until we hit one and then add the accumulated
+    // value to our list of ration totals
+    for (std::string line; std::getline(input, line); ) {
+        if (line.length() == 0) {
+            rations.push_back(ration_accumulator);
+            ration_accumulator = 0;
+        } else {
+            ration_accumulator += std::stoi(line);
+        }
+    }
+
+    std::cout << "The elf with the fattest pack is carrying "
+              << *std::max_element(rations.begin(), rations.end())
+              << " calories"
+              << std::endl;
+}
